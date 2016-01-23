c57e0942299fbf7ed84275a784c96aa71254bc2c
TakaoObi <takao@obi.jp>
2016-01-23 20:37:13 +0900

    Guister ver2

--- Geisters.py
+++ Geisters.py
@@ -14,19 +14,122 @@ myblueImg = pygame.image.load("myblue.png").convert()
 enemyredImg = pygame.image.load("otherred.png").convert()
 enemyblueImg = pygame.image.load("otherblue.png").convert()
 
-board_mass = [0] * 36
+#ボードのマス
+board = [0] * 36
 
-turn_count = 0
+#ターンのカウント
+Turn_Count = 0
 
-click_mass = 0
-click_check = 0
+#クリックされたマス
+Click_Square = 0
 
-game_turn = 0
+#クリックされた駒の周りの座標格納用, 0:上,1:左,2:右,3:下
+Move_Squares = [0] *4
 
-sysfont = pygame.font.SysFont(None, 80)
+#クリックされたかのフラグ
+Click_flag = 0
 
+#自分、相手の駒の数字格納用変数
+My_red = 1
+My_blue = 2
+Enemy_red = 3
+Enemy_blue = 4
+
+#取った駒の数の格納用変数
+My_red_count = 0
+My_blue_Count = 0
+Enemy_red_Count = 0
+Enemy_blue_Count = 0
+
+#ゲームのターン,偶数で自分,奇数で相手
+Game_turn = 0
+
+#マス移動時、その前のクリックしたマスを保存するための変数
+My_Click_Piece = 0
+
+#移動するかのフラグ
+Move_flag = 0
+
+#勝利条件用フラグ
+Win_flag = 0
+
+#マウスのx,y座標
 x = 0
 y = 0
+
+#カウント用変数（繰り返し利用可能?）
+Counter = 0
+
+#文字表示用のフォント
+sysfont = pygame.font.SysFont(None, 80)
+
+#以下デバッグ用
+#あれば付け足してください
+
+
+
+
+def Set_Pieces(boards = [0]*36, Click_Squares = 0, Counter = 0, Game_turn = 0):
+
+    if Game_turn == 0:
+        
+        if Counter < 4 and (24 < Click_Squares <29 or 30< Click_Squares <35)\
+           and boards[Click_Squares] == 0:
+            
+                boards[Click_Squares] = My_red
+                Click_flag = 0
+                Counter += 1
+
+        elif (24 < Click_Squares <29 or 30< Click_Squares <35)\
+             and boards[Click_Squares] == 0:
+                boards[Click_Squares] = My_blue
+                Click_flag = 0
+                Counter += 1
+                if Counter == 8:
+                    Counter = 0
+                    Game_turn = 1                  
+        else:
+                Click_flag = 0
+
+    elif Game_turn == 1:
+        
+        if Counter < 4 and (0 < Click_Squares < 5 or 6 < Click_Squares < 11) \
+           and boards[Click_Squares] == 0:
+            
+                boards[Click_Squares] = My_red
+                Click_flag = 0
+                Counter += 1
+
+        elif (0 < Click_Squares < 5 or 6 < Click_Squares < 11) \
+             and boards[Click_Squares] == 0:
+                boards[Click_Squares] = My_blue
+                Click_flag = 0
+                Counter += 1
+                if Counter == 8:
+                    Counter = 0
+                    Game_turn = 2                  
+        else:
+                Click_flag = 0
+        
+
+    return Counter, Click_flag, Game_turn
+
+
+def Click_Squares(x = 0, y = 0):
+    if(x < 700 and y < 700):
+        Click_Square = int (((x - 100) // 100) + 6 * ((y - 100) // 100))
+
+    return Click_Square
+        
+
+#def Move_Piece(Click_Square, board):
+    
+
+#def Check_Squares(board, Click_Square):
+    
+
+#def Win_Check(Enemy_red_Count, Enemy_blue_Count):
+    
 
 
 
@@ -34,40 +137,53 @@ while True:
     screen.fill((255,255,255))
 
     screen.blit(boardImg, (100,100))
+
+    #以下クリックしたマス、座標の表示
     
-    hello1 = sysfont.render(str(click_mass) , False, (0,0,0))
+    hello1 = sysfont.render(str(Click_Square) , False, (0,0,0))
     
     screen.blit(hello1, (10,50))
-
 
     hello2 = sysfont.render(str(x) , False, (0,0,0))
     hello3 = sysfont.render(str(y) , False, (0,0,0))
     screen.blit(hello2, (10,100))
     screen.blit(hello3, (10,150))
 
-    if game_turn == 0 and click_check == 1:
-        if turn_count < 5 and (24 < click_mass <29 or 30<click_mass<35):
-            board_mass[int(click_mass)] = 1
-            click_check = 0
-        elif 24 < click_mass <29 or 30<click_mass<35:
-            board_mass[int(click_mass)] = 2
-            click_check = 0
-            if turn_count == 8:
-                turn_count = 0
-                game_turn = 1
-        else:
-            click_check = 0
-            turn_count -= 1
+    if Game_turn < 2:
+        if Click_flag == 1:
+            Counter, Click_flag, Game_turn = \
+                     Set_Pieces(board, Click_Square, Counter, Game_turn)
 
-    hello4 = sysfont.render(str(turn_count) , False, (0,0,0))
-    screen.blit(hello4, (10,200))
+#    else:
+#        Move_Piece(Click_Square, board)
+
+#        Win_Check(Enemy_red_Count, Enemy_blue_Count)
+
+    if(Game_turn % 2 == 1):
+        My_red = 3
+        My_blue = 4
+        Enemy_red = 1
+        Enemy_blue = 2
         
 
+    if Move_flag == 1:
+        for i in range(4):
+            if Move_Squares[i] is not None:
+                pygame.draw.rect(screen, (255,255,0), \
+                                 Rect(100 + Move_Squares[i] % 6 * 100, \
+                                      100 + Move_Squares[i] // 6 * 100, \
+                                      200 + Move_Squares[i] % 6 * 100, \
+                                      200 + Move_Squares[i] // 6 * 100))
+
     for i in range(36):
-        if board_mass[i] == 1:
+        if board[i] == 1:
             screen.blit(myredImg, (100 + i%6*100, 100 + i//6*100))
-        elif board_mass[i] == 2:
+        elif board[i] == 2:
             screen.blit(myblueImg, (100 + i%6*100, 100 + i//6*100))
+        elif board[i] == 3:
+            screen.blit(enemyredImg, (100 + i%6*100, 100 + i//6*100))
+        elif board[i] == 4:
+            screen.blit(enemyblueImg, (100 + i%6*100, 100 + i//6*100))
 
 
     pygame.display.update()
@@ -78,7 +194,7 @@ while True:
 
         
         if event.type == MOUSEBUTTONDOWN and event.button == 1:
-            click_check = 1
+            Click_flag = 1
             x, y = pygame.mouse.get_pos()
-            click_mass = int(((x - 100) // 100) + 6 * ((y - 100) // 100))
-            turn_count += 1
+            Click_Square = Click_Squares(int(x), int(y))
+            
